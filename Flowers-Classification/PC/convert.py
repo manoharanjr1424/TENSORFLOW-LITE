500ee9a3605a1a7207a37d065e2673068feb1ddd
MANOHARAN JR <106403104+manoharanjr1424@users.noreply.github.com>
2025-06-25 11:45:09 +0530

    Update convert.py

--- Flowers-Classification/PC/convert.py
+++ Flowers-Classification/PC/convert.py
@@ -1,15 +1,15 @@
 import tensorflow as tf
 
 # Step 1: Load the Keras H5 model
-model = tf.keras.models.load_model('/home/mjayakumar/VVDN_BU_PROJECTS/tensorflowlite/Flowers Recognition/flowersOption2.h5')
+model = tf.keras.models.load_model('flowersOption2.h5')
 
 # Step 2: Convert the model to TFLite
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
 # Step 3: Save the TFLite model
-with open('/home/mjayakumar/VVDN_BU_PROJECTS/tensorflowlite/Flowers Recognition/flowersOption2.tflite', 'wb') as f:
+with open('flowersOption2.tflite', 'wb') as f:
     f.write(tflite_model)
 
-print("✅ Converted successfully to TFLite format!")
+print("Converted successfully to TFLite format!")
 
